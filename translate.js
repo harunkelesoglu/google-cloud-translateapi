79a3b59f98a1afcff8d837b2b7bbdca4e44ca8d1
Harun KELEŞOĞLU <harunkelesoglu_@hotmail.com>
2017-04-19 16:34:48 +0300

    firs commit

---
+++ translate.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+var sys = require('util')
+const exec = require('child_process').exec;
+const notifier = require('node-notifier');
+const path = require('path');
+const Translate = require('@google-cloud/translate');
+
+
+
+const translateClient= Translate({
+  projectId: "" // Buraya projectid yi yazacaksınız
+});
+
+
+// xclip clipboarda bulunan texti öğrenmeniz için kullanılan bir komuttur
+//Eğer sisteminizde yoksa `sudo apt-get install xclip` komutu ile kurabilirsiniz
+exec("xclip -o", function (error, stdout, stderr) {
+ if(error) throw error;
+translateClient.detect(stdout).then((results)=>{
+	let lang=results[0].language;
+	let target;
+	if(lang=="tr")
+		target="en";
+	else
+		target="tr"
+
+	translateClient.translate(stdout,target)
+				   .then(function(results){
+				   		run(stdout,results[0]);
+				   });
+
+});
+
+ 
+
+});
+
+function run(text,translate){
+	
+notifier.notify({
+  title: 'GNU Translator',
+  message: text+" - "+translate,
+  icon: path.join('/home/fr33/Resimler/linux.png'), // Buraya istediğini bir icon yolunu belirtebilirsiniz
+  
+}, function (err, response) {
+  if(err) throw err;
+  console.log(response);
+});
+}
+
+
+
+
